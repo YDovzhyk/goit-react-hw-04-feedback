ffeca5a15848fe2846559818218c5ea044ca8312
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2022-08-26 15:24:38 +0300

    add hw
    add hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import FeedbackOptions from "./FeedbackForm/FeedbackOptions/FeedbackOptions";
 import Statistics from "./FeedbackForm/Statistics/Statistics";
@@ -7,53 +7,45 @@ import NotificationMessage from "./FeedbackForm/NotificationMessage/Notification
 
 import s from "./FeedbackForm/feedbackForm.module.css"
 
-
-export class App extends Component {
-    state = {
+export const App = () => {
+    const [state, setState] = useState({
         good: 0,
         neutral: 0,
         bad: 0
-    }
+    });
 
-    countTotalFeedback() {
-        const {good, neutral, bad} = this.state;
-        return good + neutral + bad;
+    const onLeaveFeedback = (propertyName) => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                [propertyName]: prevState[propertyName] + 1
+            }
+        });
     }
+    const {good, neutral, bad} = state;
+    const totalFeedback = good + neutral + bad;
 
-    countPositiveFeedbackPercentage(propertyName) {
-        const total = this.countTotalFeedback();
-        if(!total) {
+    const countPercentage = (propertyName) => {
+        if(!totalFeedback) {
             return 0;
         }
-        const value = this.state[propertyName];
-        const result = (value / total) * 100;
+        const value = state[propertyName];
+        const result = (value / totalFeedback) * 100;
         return Number(result.toFixed(2));
     }
 
-    onLeaveFeedback = propertyName => {
-        this.setState(prevState => ({
-            [propertyName]: prevState[propertyName] + 1,
-        }));
-    }
+    const goodPercent = countPercentage("good");
 
-    render(){
-        const {good, neutral, bad} = this.state;
-
-        const total = this.countTotalFeedback();
-
-        const goodPercent = this.countPositiveFeedbackPercentage("good");
-
-        return (
-            <div className={s.feedbackForm}>
-                <SectionTitle title="Please leave feedback">
-                    <FeedbackOptions options={this.state} onLeaveFeedback={this.onLeaveFeedback} />
-                </SectionTitle>
-                <SectionTitle title="Statistics">
-                    {total > 0 ? <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={goodPercent}></Statistics> : <NotificationMessage title="There is no feedback"></NotificationMessage>}
-                </SectionTitle>
-            </div>
-        )
-    }
+    return (
+        <div className={s.feedbackForm}>
+            <SectionTitle title="Please leave feedback">
+                <FeedbackOptions options={state} onLeaveFeedback={onLeaveFeedback} />
+            </SectionTitle>
+            <SectionTitle title="Statistics">
+                {totalFeedback > 0 ? <Statistics good={good} neutral={neutral} bad={bad} total={totalFeedback} positivePercentage={goodPercent}></Statistics> : <NotificationMessage title="There is no feedback"></NotificationMessage>}
+            </SectionTitle>
+        </div>
+    )
 }
 
 export default App;
